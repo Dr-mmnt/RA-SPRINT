d024e736b017d1d32d8e0afd92190120922c0629
Sergei Musin <mmnt98@mail.ru>
2023-05-07 13:46:55 +0700

    slick

--- src/js/index.js
+++ src/js/index.js
@@ -1,95 +1,104 @@
-class TabItem { 
-  constructor(link, content) { 
-    this.link = link; 
-    this.content = content; 
-  } 
-   
-  onClick(callback) { 
-    this.link.addEventListener('click', () => callback()); 
-  } 
-   
-  activate() { 
-    this._toggle(true);   
-  } 
-   
-  deactivate() { 
-    this._toggle(false); 
-  } 
-   
-  _toggle(activate) { 
-    this.link.classList.toggle('active', activate); 
-    this.content.classList.toggle('active', activate); 
-  } 
-} 
+class TabItem {
+  constructor(link, content) {
+    this.link = link;
+    this.content = content;
+  }
 
-class TabsManager { 
-  constructor(tabsElem) { 
-    this.tabs = []; 
-    this.activeTab = null; 
-     
-    this.init(tabsElem);     
-    this.activateTab(this.tabs[0]); 
-  } 
-   
-  init(tabsElem) { 
-    const links = tabsElem.querySelectorAll('.tabs__links li'); 
-    const contents = tabsElem.querySelectorAll('.tabs__item'); 
-         
-    for (let i = 0; i < links.length; i++) { 
-      const tab = new TabItem(links[i], contents[i]); 
-      this.tabs.push(tab); 
-       
-      tab.onClick(() => this.activateTab(tab));   
-    } 
-  } 
-   
-  activateTab(tab) { 
-    if (this.activeTab) { 
-      this.activeTab.deactivate(); 
-    } 
-    this.activeTab = tab; 
-    this.activeTab.activate(); 
-  } 
-} 
+  onClick(callback) {
+    this.link.addEventListener("click", () => callback());
+  }
 
-window.onload = () => { 
-    btn.addEventListener('click', () => { 
-      mobileMenu.classList.add('visible'); 
-    }); 
-     
-    mobileCloseBtn.addEventListener('click', () => { 
-      mobileMenu.classList.remove('visible'); 
-    });   
+  activate() {
+    this._toggle(true);
+  }
 
-    const header = document.querySelector('.mobileNavigation__list'); 
-     
-    document.addEventListener('scroll', event => { 
-        console.log(event.target.scrollTop) 
-    }); 
-     
-    const smoothScrollLinks = document.querySelectorAll('.smooth-scroll'); 
-     
-    for (let link of smoothScrollLinks) { 
-      link.addEventListener('click', event => { 
-         event.preventDefault(); 
-         
-        const target = event.target; 
-        const elementToScroll = document.querySelector(target.getAttribute('href')); 
-        elementToScroll.scrollIntoView({ behavior: 'smooth', block: 'end'}); 
-        mobileMenu.classList.remove('visible');
-      }); 
-    } 
+  deactivate() {
+    this._toggle(false);
+  }
 
-  const tabsElem = document.getElementById('myTabs'); 
-  new TabsManager(tabsElem); 
+  _toggle(activate) {
+    this.link.classList.toggle("active", activate);
+    this.content.classList.toggle("active", activate);
+  }
+}
 
-    $('.slider').slick({
-      // prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/arrowLeft.svg'>",
-      // nextArrow:"<img class='a-right control-c next slick-next' src='../images/arrowRight.svg'>",
-      // appendArrows: $('.paginator'),
-    arrows: true,
-    slidesToShow: 3
-        });
-  };
+class TabsManager {
+  constructor(tabsElem) {
+    this.tabs = [];
+    this.activeTab = null;
+
+    this.init(tabsElem);
+    this.activateTab(this.tabs[0]);
+  }
+
+  init(tabsElem) {
+    const links = tabsElem.querySelectorAll(".tabs__links li");
+    const contents = tabsElem.querySelectorAll(".tabs__item");
+
+    for (let i = 0; i < links.length; i++) {
+      const tab = new TabItem(links[i], contents[i]);
+      this.tabs.push(tab);
+
+      tab.onClick(() => this.activateTab(tab));
+    }
+  }
+
+  activateTab(tab) {
+    if (this.activeTab) {
+      this.activeTab.deactivate();
+    }
+    this.activeTab = tab;
+    this.activeTab.activate();
+  }
+}
+
+window.onload = () => {
+  btn.addEventListener("click", () => {
+    mobileMenu.classList.add("visible");
+  });
+
+  mobileCloseBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("visible");
+  });
+
+  const header = document.querySelector(".mobileNavigation__list");
+
+  document.addEventListener("scroll", (event) => {
+    console.log(event.target.scrollTop);
+  });
+
+  const smoothScrollLinks = document.querySelectorAll(".smooth-scroll");
+
+  for (let link of smoothScrollLinks) {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const target = event.target;
+      const elementToScroll = document.querySelector(
+        target.getAttribute("href")
+      );
+      elementToScroll.scrollIntoView({ behavior: "smooth", block: "end" });
+      mobileMenu.classList.remove("visible");
+    });
+  }
+
+  const tabsElem = document.getElementById("myTabs");
+  new TabsManager(tabsElem);
+
+  $(".slider:not(.slick-cloned)").slick({
+    prevArrow: $(".paginator__button_left"),
+    nextArrow: $(".paginator__button_right"),
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // infinite:false
+  });
+
+  Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+  });
 
   
+  
+};
